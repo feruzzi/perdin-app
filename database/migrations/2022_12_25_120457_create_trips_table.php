ef8cd6113ffc9ddb26cd772d9d2af9f2f03af9f7
Feruzzi Graha <feruzzig@gmail.com>
2022-12-25 19:18:06 +0700

    add coloumn process by on trips table

--- database/migrations/2022_12_25_120457_create_trips_table.php
+++ database/migrations/2022_12_25_120457_create_trips_table.php
@@ -21,9 +21,11 @@ return new class extends Migration
             $table->date('start_date');
             $table->date('end_date');
             $table->string('status');
+            $table->string('process_by');
             $table->foreign('username')->references('username')->on('users');
             $table->foreign('origin')->references('id_city')->on('cities');
             $table->foreign('destination')->references('id_city')->on('cities');
+            $table->foreign('process_by')->references('username')->on('users');
             $table->timestamps();
         });
     }
