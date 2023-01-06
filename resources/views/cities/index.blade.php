139fceb8f11975a1d2adf05960c0bf3ce81d3fa1
Feruzzi Graha <feruzzig@gmail.com>
2023-01-06 16:31:19 +0700

    adjust link href on dashboard, remove dashboard menu on user

--- resources/views/cities/index.blade.php
+++ resources/views/cities/index.blade.php
@@ -29,7 +29,7 @@
             <div class="col-12 col-md-6 order-md-2 order-first">
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="{{ url('dashboard/users') }}">Kota</a></li>
+                        <li class="breadcrumb-item"><a href="{{ url('dashboard/cities') }}">Kota</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Kota</li>
                     </ol>
                 </nav>
--- resources/views/layouts/components/c-user.blade.php
+++ resources/views/layouts/components/c-user.blade.php
@@ -1,12 +1,7 @@
 <ul class="menu">
     <li class="sidebar-title">Menu</li>
 
-    <li class="sidebar-item">
-        <a href="#" class='sidebar-link'>
-            <i class="bi bi-grid-fill"></i>
-            <span>Dashboard</span>
-        </a>
-    </li>
+
     <li class="sidebar-item  {{ $set_active == 'perdinku' ? 'active' : '' }}">
         <a href="{{ url('perdinku') }}" class='sidebar-link'>
             <i class="icon dripicons dripicons-stack"></i>
